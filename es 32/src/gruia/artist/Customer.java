63becbc56fc78164acecc436341831576b8d47b0
DoomBrg <robert.gruia0601@gmail.com>
2023-05-23 10:58:46 +0200

    Classes complete

--- es 32/src/gruia/artist/Customer.java
+++ es 32/src/gruia/artist/Customer.java
@@ -1,4 +1,38 @@
 package gruia.artist;
-public class Customer {
-    
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
+public class Customer extends Thread {
+    private int customerId;
+    private Semaphore chairsSemaphore;
+
+    public Customer(int customerId, Semaphore chairsSemaphore) {
+        this.customerId = customerId;
+        this.chairsSemaphore = chairsSemaphore;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println("Cliente " + customerId + " è arrivato.");
+            if (chairsSemaphore.tryAcquire(MAX_WAIT_TIME)) {
+                // Il cliente ha ottenuto una sedia e può procedere per il ritratto
+                System.out.println("Cliente " + customerId + " ha ottenuto una sedia.");
+                getPortrait();
+                System.out.println("Cliente " + customerId + " ha terminato il ritratto.");
+                chairsSemaphore.release();
+            } else {
+                // Il cliente ha aspettato troppo e rinuncia al ritratto
+                System.out.println("Cliente " + customerId + " ha aspettato troppo e rinuncia al ritratto.");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getPortrait() throws InterruptedException {
+        Random random = new Random();
+        int portraitTime = random.nextInt(5000) + 1000; // Tempo di esecuzione del ritratto (tra 1 e 5 secondi)
+        Thread.sleep(portraitTime);
+    }
 }
--- es 32/src/gruia/artist/StreetArtistSimulation.java
+++ es 32/src/gruia/artist/StreetArtistSimulation.java
@@ -1,4 +1,23 @@
-package gruia.artist;
+package gruia.artist;   
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
 public class StreetArtistSimulation {
+    private static final int NUM_CHAIRS = 4; // Numero di sedie disponibili
+    private static final int MAX_WAIT_TIME = 5000; // Tempo massimo di attesa per una sedia (in millisecondi)
+
+    private Semaphore chairsSemaphore = new Semaphore(NUM_CHAIRS);
+
+    public void startSimulation() {
+        Random random = new Random();
+        int numCustomers = random.nextInt(10) + 1; // Numero casuale di clienti (tra 1 e 10)
+
+        System.out.println("Numero di clienti: " + numCustomers);
+
+        for (int i = 1; i <= numCustomers; i++) {
+            Customer customer = new Customer(i, chairsSemaphore);
+            customer.start();
+        }
+    }
     
 }
